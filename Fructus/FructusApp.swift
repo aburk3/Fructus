566e0f4e5f4e6cb13a46f23b83f5128360cbe80d
Austin Burke <austinlayneburke@gmail.com>
2021-11-16 07:25:06 -0600

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Austin Burke on 11/16/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
