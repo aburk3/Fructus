1771ef1797e1b17f9ba6b4ee6aed7ee12baae746
Austin Burke <austinlayneburke@gmail.com>
2021-11-23 06:07:12 -0600

    Created settings view that can be closed via dismiss gesture or button

---
+++ Fructus/App/SettingsView.swift
@@ -0,0 +1,45 @@
+//
+//  SettingsView.swift
+//  Fructus
+//
+//  Created by Austin Burke on 11/23/21.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    // MARK: - PROPERTIES
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(spacing: 20) {
+                    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                } //: VSTACK
+                .navigationBarTitle(Text("Settings"), displayMode: .large)
+                .navigationBarItems(
+                    trailing:
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Image(systemName: "xmark")
+                    }
+                )
+                .padding()
+            } //: SCROLL
+        } //: NAVIGATION
+    }
+}
+
+// MARK: - PREVIEW
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+            .preferredColorScheme(.dark)
+    }
+}
